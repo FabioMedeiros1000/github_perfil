03486ed385bc6e16b2d9f03a0036b5029b210602
FabioMedeiros1000 <fabio.leandro.medeiros@gmail.com>
2024-08-06 11:41:49 -0300

    Tratando erros

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 import Perfil from "./components/Perfil";
-import Formulario from "./components/Formulario";
+// import Formulario from "./components/Formulario";
 import ReposList from "./components/ReposList";
 
 function App() {
-  const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
+  // const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
   const [nomeUsuario, setNomeUsuario] = useState(''); 
 
   return(
--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -5,41 +5,65 @@ import styles from './ReposList.module.css'
 const ReposList = ({ nomeUsuario }) => {
     const [repos, setRepos] = useState([]);
     const [estaCarregando, setEstaCarregando] = useState(true);
+    const [deuErro, setDeuErro] = useState(false);
 
     useEffect(() => {
         setEstaCarregando(true);
         fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
-        .then(res => res.json())
+        .then(res => {
+            if (!res.ok){
+                throw new Error('');
+            }
+            return res.json()
+        })
         .then(resJson => {  
+            setDeuErro(false);
             setTimeout(() => {
                 setEstaCarregando(false);
                 setRepos(resJson)
             }, 3000)
         })
+        .catch(erro => {
+            setDeuErro(true);
+        })
+        
     }, [nomeUsuario]);
 
-    return (
-        <div className="container">
-            {estaCarregando ? (
-                <h1>Carregando...</h1>
-            ) : (
-                <ul className={styles.list}>
-                    {/* {repos.map(repositorio => ( */}
-                    {repos.map(({ id, name, language, html_url }) => (
-                        <li className={styles.listItem} key={id}>
-                            <div className={styles.itemName}>
-                                <b>Nome: </b> {name} 
-                            </div>
-                            <div className={styles.itemName}>
-                                <b>Linguagem: </b> {language} 
-                            </div>
-                            <a className={styles.itemLink} target="_blank" href={html_url}>Visitar no Github</a> 
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </div>
-    )
+    if (deuErro){
+        return(
+            <div className="container">
+                <h1>Digite um nome de usuário que seja válido!</h1>
+            </div>
+        )
+    }
+
+    else{
+        return (
+            <div className="container">
+                {estaCarregando ? (
+                    <h1>Carregando...</h1>
+                ) : (
+                    <ul className={styles.list}>
+                        {/* {repos.map(repositorio => ( */}
+                        {repos.map(({ id, name, language, html_url }) => (
+                            <li className={styles.listItem} key={id}>
+                                <div className={styles.itemName}>
+                                    <b>Nome: </b> {name} 
+                                </div>
+                                <div className={styles.itemName}>
+                                    <b>Linguagem: </b> {language} 
+                                </div>
+                                <a className={styles.itemLink} target="_blank" href={html_url}>Visitar no Github</a> 
+                            </li>
+                        ))}
+                    </ul>
+                )}
+                
+            </div>
+        )
+    }
+
+
 }
 
 export default ReposList;
